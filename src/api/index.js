b8655ddff96a67ba86c721ab15e1b411fd6dfd5b
Namkyu Won <64752079+Nwon2464@users.noreply.github.com>
2024-05-11 16:52:11 +0900

    Update index.js
    d

--- src/api/index.js
+++ src/api/index.js
@@ -152,110 +152,139 @@ router.get("/twitch", async (req, res) => {
   res.send(data);
 
   // console.log(client_id);
-  // try {
-  //   const response = await axios.post(
-  //     `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=client_credentials`
-  //   );
-  //   const token = response.data.access_token;
-  //   const options = {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //       "client-id": client_id,
-  //     },
-  //   };
+  try {
+    const response = await axios.post(
+      `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=client_credentials`
+    );
+    const token = response.data.access_token;
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "client-id": client_id,
+      },
+    };
 
-  //   console.log(token, "token");
-  //   if (token) {
+    console.log(token, "token");
+    if (token) {
     
-  //     console.log("WERWEREWREWR",token);
-  //     const getStreamsRequest = await axios.get(
-  //       "https://api.twitch.tv/helix/streams?first=5",
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //           "client-id": client_id,
-  //         },
-  //       }
-  //     ).then(e=>{
-  //         const newStreamsData = e.data.data;
-  //         let allStreams = newStreamsData.slice();
-  //         let urls= [],user_urls=[];
-  //         // // console.log(newStreamsData);
-  //         for(let i =0;i<5;i++ ) {
-  //           urls.push(`https://api.twitch.tv/helix/channels?broadcaster_id=${newStreamsData[i].user_id}`);  
-  //           user_urls.push(`https://api.twitch.tv/helix/users?id=${newStreamsData[i].user_id}`);
-  //         //   // user_tags.push(`https://api.twitch.tv/helix/tags/streams`);
-  //         }
+      console.log("WERWEREWREWR",token);
+      const getStreamsRequest = await axios.get(
+        "https://api.twitch.tv/helix/streams?first=5",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "client-id": client_id,
+          },
+        }
+      ).then(e=>{
+          const newStreamsData = e.data.data;
+          let allStreams = newStreamsData.slice();
+          let urls= [],user_urls=[];
+          // // console.log(newStreamsData);
+          for(let i =0;i<5;i++ ) {
+            urls.push(`https://api.twitch.tv/helix/channels?broadcaster_id=${newStreamsData[i].user_id}`);  
+            user_urls.push(`https://api.twitch.tv/helix/users?id=${newStreamsData[i].user_id}`);
+          //   // user_tags.push(`https://api.twitch.tv/helix/tags/streams`);
+          }
       
-  //         const promise1 = axios.get(urls[0], options);
-  //         const promise2 = axios.get(urls[1], options);
-  //         const promise3 = axios.get(urls[2], options);
-  //         const promise4 = axios.get(urls[3], options);
-  //         const promise5 = axios.get(urls[4], options);
+          const promise1 = axios.get(urls[0], options);
+          const promise2 = axios.get(urls[1], options);
+          const promise3 = axios.get(urls[2], options);
+          const promise4 = axios.get(urls[3], options);
+          const promise5 = axios.get(urls[4], options);
           
-  //         const promiseUser1 = axios.get(user_urls[0], options);
-  //         const promiseUser2 = axios.get(user_urls[1], options);
-  //         const promiseUser3 = axios.get(user_urls[2], options);
-  //         const promiseUser4 = axios.get(user_urls[3], options);
-  //         const promiseUser5 = axios.get(user_urls[4], options);
+          const promiseUser1 = axios.get(user_urls[0], options);
+          const promiseUser2 = axios.get(user_urls[1], options);
+          const promiseUser3 = axios.get(user_urls[2], options);
+          const promiseUser4 = axios.get(user_urls[3], options);
+          const promiseUser5 = axios.get(user_urls[4], options);
           
-  //         axios
-  //         .all([
-  //           promise1,
-  //           promise2,
-  //           promise3,
-  //           promise4,
-  //           promise5,
-  //           promiseUser1,
-  //           promiseUser2,
-  //           promiseUser3,
-  //           promiseUser4,
-  //           promiseUser5,
-  //         ]).then(
-  //           axios.spread((...response) => {
-  //             let gameName = [];
-  //             let imageUrl = [];
-  //             let tags = [];
-  //             let description=[];
-  //             response.map((data, i) => {
+          axios
+          .all([
+            promise1,
+            promise2,
+            promise3,
+            promise4,
+            promise5,
+            promiseUser1,
+            promiseUser2,
+            promiseUser3,
+            promiseUser4,
+            promiseUser5,
+          ]).then(
+            axios.spread((...response) => {
+              let gameName = [];
+              let imageUrl = [];
+              let tags = [];
+              let description=[];
+              response.map((data, i) => {
               
-  //               data.data.data.map((res)=>{
-  //                 if(res.hasOwnProperty("broadcaster_language")){
-  //                   tags.push({
-  //                     localization_names:res["broadcaster_language"]
-  //                   })
-  //                 }
-  //                 if (res.hasOwnProperty("profile_image_url")) {
-  //                   imageUrl.push({
-  //                     profile_image_url: res["profile_image_url"],
-  //                   });
-  //                 }
-  //                 if (res.hasOwnProperty("description")) {
-  //                   description.push({
-  //                     description: res["description"],
-  //                   });
-  //                 }
-  //                 if (res.hasOwnProperty("game_id")) {
-  //                   gameName.push({ game_name: res.game_name });
-  //                 }
-  //               })
-  //             });
-  //             _.merge(allStreams, imageUrl);
-  //             _.merge(allStreams, gameName);
-  //             _.merge(allStreams, tags);
-  //             _.merge(allStreams, description);
-  //             res.send(
-  //               allStreams
-  //             );
-  //           })
-  //         );
-  //     });
-  //   }
-  // } catch (error) {
-  //   console.log("ERROR206");
-  // }
+                data.data.data.map((res)=>{
+                  if(res.hasOwnProperty("broadcaster_language")){
+                    tags.push({
+                      localization_names:res["broadcaster_language"]
+                    })
+                  }
+                  if (res.hasOwnProperty("profile_image_url")) {
+                    imageUrl.push({
+                      profile_image_url: res["profile_image_url"],
+                    });
+                  }
+                  if (res.hasOwnProperty("description")) {
+                    description.push({
+                      description: res["description"],
+                    });
+                  }
+                  if (res.hasOwnProperty("game_id")) {
+                    gameName.push({ game_name: res.game_name });
+                  }
+                })
+              });
+              _.merge(allStreams, imageUrl);
+              _.merge(allStreams, gameName);
+              _.merge(allStreams, tags);
+              _.merge(allStreams, description);
+              console.log(allStreams);
+              // res.send(
+              //   allStreams
+              // );
+            })
+          );
+      });
+    }
+  } catch (error) {
+    console.log("ERROR206");
+  }
 });
 
+
+//try experimenting fetching archives for specfic users with users id  
+router.get("/et",async(req,res)=>{
+
+  const response = await axios.post(
+    `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=client_credentials`
+  );
+  const token = response.data.access_token;
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "client-id": client_id,
+    },
+  };
+  const getStreamsRequest = await axios.get(
+    "https://api.twitch.tv/helix/videos?id=2127617160",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "client-id": client_id,
+      },
+    }
+  ).then(e=>{
+      const newStreamsData = e.data.data;
+    console.log(newStreamsData);
+    });
+
+})
 
 
 router.get("/twitch/categories/all", async (req, res) => {
