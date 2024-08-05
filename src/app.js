53a9087573a048fddf89f40c99cd6bde7f28b465
Namkyu Won <64752079+Nwon2464@users.noreply.github.com>
2024-08-06 01:40:56 +0900

    Revert "passport oauth"
    This reverts commit fd405629977dc874564b2aa17d4a472b3ee37b5b.

--- src/app.js
+++ src/app.js
@@ -4,12 +4,26 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const axios = require('axios');
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const authRoutes = require("./auth");
+require("./config");
 
 
 const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, //1day for authorized cookie
+    keys: [process.env.COOKIE_KEY],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(morgan('dev'));
 app.use(helmet());
@@ -22,6 +36,7 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use("/auth", authRoutes);
 app.use('/api/v1', api);
 
 app.use(middlewares.notFound);
--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -1,29 +1,29 @@
-// const jwt = require("jsonwebtoken");
-// // const JWT_SECRET
-// const checkTokenSetUser = (req, res, next) => {
-//   const authHeader = req.get("authorization");
-//   if (authHeader) {
-//     const token = authHeader.split(" ")[1];
-//     if (token) {
-//       console.log("yes");
-//       jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
-//         if (error) {
-//           console.log("errorNAME ===", error.name);
-//           console.log(error);
+const jwt = require("jsonwebtoken");
+// const JWT_SECRET
+const checkTokenSetUser = (req, res, next) => {
+  const authHeader = req.get("authorization");
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    if (token) {
+      console.log("yes");
+      jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+        if (error) {
+          console.log("errorNAME ===", error.name);
+          console.log(error);
           
-//         }
-//         console.log("--->", user);
-//         req.user = user;
-//         next();
-//       });
-//     } else {
-//       next();
-//     }
-//   } else {
-//     next();
-//   }
-// };
+        }
+        console.log("--->", user);
+        req.user = user;
+        next();
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+};
 
-// module.exports = {
-//   checkTokenSetUser,
-// };
+module.exports = {
+  checkTokenSetUser,
+};
